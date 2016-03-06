7da3e18445290c15276883e8a933d515649ed618
Jan <jackflash5500@gmail.com>
2016-03-06 11:52:49 +0100

    Added refactoring from vvye

--- i2c.c
+++ i2c.c
@@ -1,80 +1,100 @@
-//*************************************************
-//** Polling I2C-Master Library
-//** Created by Mike Schaub
-//** Modified by Patrick Mania <pmania_at_tzi.de>
-//** Version: 1.0
-//** Description:
-//** This Library provides I2C-Master Functions on LPC2000 Microcontrollers.
-//** The Code was tested on a LPC2124 and LPC2194 
-//** 
-//** Compiled with WinARM
-//*************************************************
-
-#include <targets/LPC2000.h>
 #include "i2c.h"
 
-// Initialize I2C
-void i2c_init(){
-        PINSEL0 |= 0x50; 
-	I2SCLL = I2SCLH = 400;	//	75kBits @ 58,98 <MHz
-	I2CONSET = 0x40;		//	enable I2C bus interface
-}
 
-// Handle timeouts and interrupt action
-static void wait_for_SI(void){
-	unsigned long timeout = 1600000;
-
-	I2CONCLR = 8;								//	clear SI starts action
-	while (timeout-- && !(I2CONSET & 8));		//	check SI with timeout
+/**
+    Initialize I2C
+*/
+void i2c_Init(void)
+{
+    PINSEL0 |= 0x50;
+    I2SCLL = I2SCLH = 400; // 75kBits @ 58,98 <MHz
+    I2CONSET = 0x40; // enable I2C bus interface
 }
 
 
-//Send "START" condition
-int i2c_start(int addr){
-	I2CONCLR = 0x14;								//	clear STO, AA
-	I2CONSET = 0x28;								//	set STA, SI
-	wait_for_SI();
-	I2CONCLR = 0x20;								//	clear STA
-	if (I2STAT > 0x10) return(-1);				//	bus is busy
-	I2DAT = addr;									//	set address
-	wait_for_SI();
-	return (I2STAT != 0x40 && I2STAT != 0x18);	//  no acknowledge
+/**
+    Handle timeouts and interrupt action
+*/
+static void i2c_WaitForSI(void)
+{
+    unsigned long timeout = 1600000;
+    I2CONCLR = 8; // clear SI starts action
+    while (timeout-- && !(I2CONSET & 8)); // check SI with timeout
 }
 
-//Write on I2C bus
-int i2c_write(unsigned char *buf, unsigned count){
-	while (count--)
-	{
-		I2DAT = *buf++;						//	load data into I2DAT-Register
-		wait_for_SI();
-		if (I2STAT != 0x28) return 1;		//	no acknowledge
-	}
-	return 0;
+
+/**
+    Send "START" condition
+*/
+int i2c_start(int addr)
+{
+    I2CONCLR = 0x14; // clear STO, AA
+    I2CONSET = 0x28; // set STA, SI
+    i2c_WaitForSI();
+
+    I2CONCLR = 0x20; // clear STA
+
+    if (I2STAT > 0x10) { // bus is busy
+        return -1;
+    }
+    I2DAT = addr; // set address
+    i2c_WaitForSI();
+
+    return (I2STAT != 0x40 && I2STAT != 0x18); // no acknowledge
 }
 
-//Read from I2C bus
-int i2c_read(){
-  I2CONSET=AA; // Assert Acknowledge
-  wait_for_SI();
-  if(I2STAT!=0x50 && I2STAT!=0x40){
-    return -1; // Not ok
-  }else{
+
+/**
+    Write to I2C bus
+*/
+int i2c_Write(unsigned char *buf, unsigned count)
+{
+    while (count--) {
+        I2DAT = *buf++; // load data into I2DAT register
+        i2c_WaitForSI();
+
+        if (I2STAT != 0x28) {
+            return 1;  // no acknowledge
+        }
+    }
+    return 0;
+}
+
+
+/**
+    Read from I2C bus
+*/
+int i2c_Read(void)
+{
+    I2CONSET = AA; // assert Acknowledge
+    i2c_WaitForSI();
+
+    if (I2STAT != 0x50 && I2STAT != 0x40) {
+        return -1; // not OK
+    }
     return I2DAT;
-  }
 }
 
-//Read last byte from I2C Bus
-int i2c_readlast(){
-  I2CONCLR=AA; // Assert Acknowledge
-  wait_for_SI();
-  if(I2STAT!=0x58){
-    return -1; // Not ok
-  }else{
+
+/**
+    Read last byte from I2C Bus
+*/
+int i2c_ReadLastByte(void)
+{
+  I2CONCLR = AA; // assert Acknowledge
+  i2c_WaitForSI();
+
+    if (I2STAT != 0x58){
+        return -1; // not OK
+    }
     return I2DAT;
-  }
 }
 
-//Send "STOP" condition
-void i2c_stop(void){	
-	I2CONSET = 0x40;		//	Re-enable I2C bus interface
+
+/**
+    Send "STOP" condition
+*/
+void i2c_stop(void)
+{
+    I2CONSET = 0x40; // re-enable I2C bus interface
 }
--- i2c.h
+++ i2c.h
@@ -1,24 +1,25 @@
-#ifndef _I2C_H
-#define _I2C_H
+#include <targets/LPC2000.h>
 
-#define STA	0x20
-#define SIC	0x08
-#define SI	0x08
-#define STO	0x10
-#define STAC 0x20
-#define AA	0x04
+#define STA     0x20
+#define SIC     0x08
+#define SI      0x08
+#define STO     0x10
+#define STAC    0x20
+#define AA      0x04
 
-#define R 1// Read Bit +1
+#define R       1
 
-// STOP Makro
-#define STOPI2C		I2C_I2CONCLR = SIC; I2C_I2CONSET=STO; while((I2C_I2CONSET&STO));
 
-void i2c_init();
-static void wait_for_SI(void);
-int i2c_start(int addr);
-int i2c_write(unsigned char *buf, unsigned count);
-int i2c_read();
-int i2c_readlast();
-void i2c_stop(void);
+void i2c_Init(void);
 
-#endif
+static void i2c_WaitForSI(void);
+
+int i2c_Start(int addr);
+
+int i2c_Write(unsigned char *buf, unsigned count);
+
+int i2c_Read(void);
+
+int i2c_ReadLast(void);
+
+void i2c_Stop(void);
